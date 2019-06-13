f31574ed49ad71e2feb7305cb9eb418c3489ccca
Michael Romero <michael.romero09@gmail.com>
2019-06-13 09:04:08 -0700

    added README.md

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 'use strict';
 
 const app = require('./app');
-
-const PORT = process.env.PORT || 8000;
+const { PORT } = require('./config');
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
